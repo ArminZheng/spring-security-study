72cfa37bfa12779bb9da2ff4571580c80cae6ef5
armin <arminzheng@qq.com>
2022-05-26 11:33:08 +0800

    fix:重大失误 BindingResult 写成 BindException fix:没有引入 spring-boot-starter-validation feat:在 controller 获取 currentUser

--- security-02/src/main/java/com/armin/security/controller/WithdrawController.java
+++ security-02/src/main/java/com/armin/security/controller/WithdrawController.java
@@ -1,8 +1,16 @@
 package com.armin.security.controller;
 
-import org.springframework.validation.BindException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Map;
 
 /**
  * WithdrawController
@@ -12,6 +20,7 @@ import org.springframework.web.context.request.WebRequest;
  */
 @RestController
 @RequestMapping("/withdraw")
+@Validated // 支持验证组 Validation Group，不支持属性校验，spring提供的
 // @CrossOrigin 整个类都适用跨域
 public class WithdrawController {
 
@@ -27,8 +36,31 @@ public class WithdrawController {
     }
 
     @PostMapping(value = "/post")
-    public String withdrawPost(@RequestParam("name") String  name, BindException exception, WebRequest quest) {
+    public String withdrawPost(@Valid @RequestBody Map<String, String> values, BindingResult bindingResult, WebRequest quest) {
         // quest.get
-        return "Withdraw Info " + name;
+        if (bindingResult.hasErrors()) {
+            throw new IllegalArgumentException("错误提示码" + bindingResult.getFieldError().getDefaultMessage());
+        }
+        return "Withdraw Info " + values + "\ncurrent: " + getCurrentUserName() + "\n exception" + bindingResult;
+    }
+
+    @GetMapping("/currentUser")
+    public Authentication getCurrentUser(Authentication authentication) {
+        return authentication;
+    }
+
+    @GetMapping("/currentUsername")
+    public String currentUserName(Principal principal) {
+        return principal.getName();
+    }
+
+    public String getCurrentUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName(); // 一般为 loginId
+            return currentUserName;
+        } else { // 解决匿名访问, 返回一个匿名用户的问题
+            throw new RuntimeException("No User");
+        }
     }
 }

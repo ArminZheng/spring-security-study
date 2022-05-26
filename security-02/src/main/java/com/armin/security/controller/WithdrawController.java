0e6ffb1d92e76af40049c2a1dce669e7f8ef5587
armin <arminzheng@qq.com>
2022-05-26 14:30:39 +0800

    feat:获取currentUser两种重要方式

--- security-02/src/main/java/com/armin/security/controller/WithdrawController.java
+++ security-02/src/main/java/com/armin/security/controller/WithdrawController.java
@@ -2,7 +2,11 @@ package com.armin.security.controller;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -36,12 +40,24 @@ public class WithdrawController {
     }
 
     @PostMapping(value = "/post")
-    public String withdrawPost(@Valid @RequestBody Map<String, String> values, BindingResult bindingResult, WebRequest quest) {
+    public String withdrawPost(
+            @Valid @RequestBody Map<String, String> values,
+            BindingResult bindingResult,
+            WebRequest quest) {
+        Principal userPrincipal = quest.getUserPrincipal();
         // quest.get
         if (bindingResult.hasErrors()) {
-            throw new IllegalArgumentException("错误提示码" + bindingResult.getFieldError().getDefaultMessage());
+            throw new IllegalArgumentException(
+                    "错误提示码" + bindingResult.getFieldError().getDefaultMessage());
         }
-        return "Withdraw Info " + values + "\ncurrent: " + getCurrentUserName() + "\n exception" + bindingResult;
+        return "Withdraw Info "
+                + values
+                + "\ncurrent: "
+                + getCurrentUserName()
+                + "\n exception"
+                + bindingResult
+                + "\n userPrincipal: "
+                + userPrincipal;
     }
 
     @GetMapping("/currentUser")
@@ -54,6 +70,9 @@ public class WithdrawController {
         return principal.getName();
     }
 
+    /*
+    ！！重要方式 1/2
+     */
     public String getCurrentUserName() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
@@ -62,5 +81,67 @@ public class WithdrawController {
         } else { // 解决匿名访问, 返回一个匿名用户的问题
             throw new RuntimeException("No User");
         }
+    }
+    /**
+     * ！！重要方式 2/2
+     * 很多时候我们自定义了用户对象UserDetails, 我们可以通过Spring Security 4.0提供的注解@AuthenticationPrincipal
+     * 来获取当前用户的自定义UserDetails对象。如果CustomUser是UserDetails的实现，那么我们可以：
+     *
+     * @param customUser
+     * @return
+     */
+    @GetMapping("/current_user")
+    public UserDetails currentUserName(@AuthenticationPrincipal UserDetails customUser) {
+        return customUser;
+    }
+
+    /**
+     * simplify
+     *
+     * @param username
+     * @return
+     */
+    @GetMapping("/current_user_name")
+    public String currentUserName(
+            @AuthenticationPrincipal(expression = "username") String username) {
+        return username;
+    }
+
+    /**
+     * Spring Security 5 提供了一个新的注解@CurrentSecurityContext来获取当前用户的安全上下文
+     *
+     * @param authentication
+     * @return
+     */
+    @GetMapping("/current__username")
+    public String currentUserName(
+            @CurrentSecurityContext(expression = "authentication") Authentication authentication) {
+        return authentication.getName();
+    }
+    /**
+     * Spring Security 5 提供了一个新的注解@CurrentSecurityContext来获取当前用户的安全上下文
+     *
+     * @param securityContext
+     * @return
+     */
+    @GetMapping("/current__username1")
+    public String currentUserName1(
+            @CurrentSecurityContext SecurityContext securityContext) {
+        Authentication authentication = securityContext.getAuthentication();
+        return authentication.getName();
+    }
+
+    /**
+     * simplify
+     *
+     * <p>通过expression参数声明SpEL表达式来获取其它属性，例如获取Principal对象
+     *
+     * @param principal
+     * @return
+     */
+    @GetMapping("/principal")
+    public String getPrincipal(
+            @CurrentSecurityContext(expression = "authentication.principal") Principal principal) {
+        return principal.getName(); // null pointer
     }
 }
